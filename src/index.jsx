465000eb7e5869e304baed8a90874f214f966dda
sebahattin <sebkucuk05@gmail.com>
2023-02-11 22:04:10 +0300

    v2

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* 🍀 This Component index.jsx */}
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
